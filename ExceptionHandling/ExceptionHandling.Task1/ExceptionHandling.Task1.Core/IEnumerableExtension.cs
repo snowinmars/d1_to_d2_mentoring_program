774195ae2ef059d7493ea1853b23615ff3aeafed
Dmitrii_Tkachenko <Dmitrii_Tkachenko@epam.com>
2016-12-01 09:33:14 +0300

    Add smoke tests for ExceptionHandling Task 1

---
+++ ExceptionHandling/ExceptionHandling.Task1/ExceptionHandling.Task1.Core/IEnumerableExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling.Task1.Core
+{
+    // ReSharper disable once InconsistentNaming
+    public static class IEnumerableExtension
+    {
+        public static IEnumerable<char> Go(this IEnumerable<string> input)
+        {
+            return null;
+        }
+    }
+}
---
+++ ExceptionHandling/ExceptionHandling.Task1/ExceptionHandling.Task1.Tests/SmokeTests.cs
@@ -0,0 +1,29 @@
+﻿using ExceptionHandling.Task1.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ExceptionHandling.Task1.Tests
+{
+    public class SmokeTests
+    {
+        [Fact]
+        public void Smoke()
+        {
+            IEnumerable<string> inputStrings = new[]
+            {
+                "a",
+                "bike",
+                "cast",
+                "doordoordoor",
+            };
+
+            IEnumerable<char> expectedResults = new[] { 'a', 'b', 'c', 'd' };
+
+            Assert.True(inputStrings.Go().SequenceEqual(expectedResults));
+        }
+    }
+}
