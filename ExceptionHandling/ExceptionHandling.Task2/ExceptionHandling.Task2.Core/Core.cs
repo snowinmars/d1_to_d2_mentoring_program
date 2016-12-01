9a7fc2608aae962f3deda3b8e515508bc175e79d
Dmitrii_Tkachenko <Dmitrii_Tkachenko@epam.com>
2016-12-01 09:38:23 +0300

    Add smoke tests for Exception Handling Task 2

---
+++ ExceptionHandling/ExceptionHandling.Task2/ExceptionHandling.Task2.Core/Core.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling.Task2.Core
+{
+    public static class Core
+    {
+        public static int Go(string inputString)
+        {
+            return -1;
+        }
+    }
+}
---
+++ ExceptionHandling/ExceptionHandling.Task2/ExceptionHandling.Task2.Tests/SmokeTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ExceptionHandling.Task2.Tests
+{
+    public class SmokeTests
+    {
+        [Fact]
+        public void A()
+        {
+            int[] ints = { 0, -1, 1, 1234, -1234, };
+            string[] strings = ints.Select(i => i.ToString()).ToArray();
+
+            for (int i = 0; i < ints.Length; i++)
+            {
+                Assert.Equal(expected: ints[i], actual: Core.Core.Go(strings[i]));
+            }
+        }
+    }
+}
