8a1a17e67306829c066dbd69ebbd724f10a783b8
CUMAJ\cutemaj <marcor@yandex.ru>
2016-12-01 22:21:38 +0300

    Wrote some tests.

--- ExceptionHandling/ExceptionHandling.Task1/ExceptionHandling.Task1.Tests/UnitTests.cs
+++ ExceptionHandling/ExceptionHandling.Task1/ExceptionHandling.Task1.Tests/UnitTests.cs
@@ -1,12 +1,56 @@
-﻿using System;
+﻿using ExceptionHandling.Task1.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Authentication;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace ExceptionHandling.Task1.Tests
 {
     public class UnitTests
     {
+        #region positive
+
+        [Theory]
+        [InlineData("1")] // 0 space
+        [InlineData(" 1")] // 1 space
+        [InlineData("    1")] // 4 spaces
+        [InlineData("1")] // 1 U+0007
+        [InlineData("1")] // 4 U+0007
+        public void TrimOnStart_MustWork(string str)
+        {
+            int actual = new[] { str }.Go().First();
+            int expected = int.Parse(str.Trim()); // IsControl
+
+            Assert.Equal(actual: actual, expected: expected);
+        }
+
+        #endregion
+
+        #region negative
+
+        [Fact]
+        public void OnNullInput_MustThrow_ArgNullExc()
+        {
+            Assert.Throws<ArgumentNullException>(() => 
+            {
+                new string[] { null }.Go();
+            });
+        }
+
+        [Fact]
+        public void OnEmptyInput_MustThrow_IndexOutOfRangeExc()
+        {
+            Assert.Throws<IndexOutOfRangeException>(() =>
+            {
+                new string[] { "" }.Go();
+            });
+        }
+
+
+
+        #endregion
     }
 }
