0ebb578c329eb1c2c0607b2ef4e19e5ef53346cf
Dmitrii_Tkachenko <Dmitrii_Tkachenko@epam.com>
2016-12-05 18:15:18 +0400

    Minor fix to smoke test of exception handling task 2

--- ExceptionHandling/ExceptionHandling.Task2/ExceptionHandling.Task2.Tests/SmokeTests.cs
+++ ExceptionHandling/ExceptionHandling.Task2/ExceptionHandling.Task2.Tests/SmokeTests.cs
@@ -6,7 +6,7 @@ namespace ExceptionHandling.Task2.Tests
     public class SmokeTests
     {
         [Fact]
-        public void A()
+        public void SmokeTest_MustWork()
         {
             int[] ints = { 0, -1, 1, 1234, -1234, };
             string[] strings = ints.Select(i => i.ToString()).ToArray();
