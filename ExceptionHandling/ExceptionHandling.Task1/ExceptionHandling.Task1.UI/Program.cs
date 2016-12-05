dee2709fb2d7128c9d5bd56b40ebcb66e613b2eb
Dmitrii_Tkachenko <Dmitrii_Tkachenko@epam.com>
2016-12-05 14:22:49 +0400

    Add UI to exception handling Task 1

--- ExceptionHandling/ExceptionHandling.Task1/ExceptionHandling.Task1.UI/Program.cs
+++ ExceptionHandling/ExceptionHandling.Task1/ExceptionHandling.Task1.UI/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace ExceptionHandling.Task1.UI
+﻿using System;
+using System.Linq;
+
+namespace ExceptionHandling.Task1.UI
 {
     internal class Program
     {
         private static void Main()
         {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                Console.WriteLine(Core.IEnumerableExtension.Go(new[] { input}).First());
+            }
         }
     }
 }
