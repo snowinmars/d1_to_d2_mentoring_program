3159a0070efa2b9230121c3f46c01bb19c822b12
Dmitrii_Tkachenko <Dmitrii_Tkachenko@epam.com>
2016-12-05 18:15:33 +0400

    Add UI impl to exception handling task 2

--- ExceptionHandling/ExceptionHandling.Task2/ExceptionHandling.Task2.UI/Program.cs
+++ ExceptionHandling/ExceptionHandling.Task2/ExceptionHandling.Task2.UI/Program.cs
@@ -1,9 +1,38 @@
-﻿namespace ExceptionHandling.Task2.UI
+﻿using System;
+
+namespace ExceptionHandling.Task2.UI
 {
     internal class Program
     {
         private static void Main()
         {
+            Console.WriteLine("Ready");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                string result;
+                try
+                {
+                    result = Core.Core.Parse(input).ToString();
+                }
+                catch (ArgumentNullException)
+                {
+                    result = "Sorry, something went wrong";
+                }
+                catch (FormatException)
+                {
+                    result = "String have invalid format";
+                }
+                catch (OverflowException)
+                {
+                    result = "Number is too big, cant fit it to the Integer range";
+                }
+
+
+                Console.WriteLine(result);
+            }
         }
     }
 }
