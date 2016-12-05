aa28dc38c9404bf0636905cf690d9e8a45e34c05
Dmitrii_Tkachenko <Dmitrii_Tkachenko@epam.com>
2016-12-05 18:53:44 +0400

    Add regression tests.

---
+++ ExceptionHandling/ExceptionHandling.Task2/ExceptionHandling.Task2.Tests/RegressionTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ExceptionHandling.Task2.Tests
+{
+    public class RegressionTests
+    {
+        [Fact]
+        public void HowToNameRegressionTests()
+        {
+            string s1 = $"+{int.MaxValue - 10}";
+            int v1 = Core.Core.Parse(s1);
+
+            string s2 = $"{int.MaxValue - 10}";
+            int v2 = Core.Core.Parse(s2);
+
+            Assert.Equal(v1, v2);
+        }
+    }
+}
