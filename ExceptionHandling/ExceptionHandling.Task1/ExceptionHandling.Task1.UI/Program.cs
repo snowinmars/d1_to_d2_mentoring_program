3aa4004b288f651e073832f425a43f731d281f92
Dmitrii_Tkachenko <Dmitrii_Tkachenko@epam.com>
2016-12-05 17:09:20 +0400

    Minor fixes to exception handling task 1 UI.

--- ExceptionHandling/ExceptionHandling.Task1/ExceptionHandling.Task1.UI/Program.cs
+++ ExceptionHandling/ExceptionHandling.Task1/ExceptionHandling.Task1.UI/Program.cs
@@ -11,7 +11,27 @@ namespace ExceptionHandling.Task1.UI
             while (true)
             {
                 string input = Console.ReadLine();
-                Console.WriteLine(input.CutOff());
+
+                string result;
+
+                try
+                {
+                    result = input.CutOff().ToString();
+                }
+                catch (FormatException)
+                {
+                    result = "Incorrect input format";
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    result = "string is too short or too long";
+                }
+                catch (ArgumentNullException)
+                {
+                    result = "Something went wrong";
+                }
+
+                Console.WriteLine(result);
             }
         }
     }
