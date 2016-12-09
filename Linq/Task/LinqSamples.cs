fac620be06a815e8fdf492797772ef4c3a8c6c0e
snowinmars <marcor@yandex.ru>
2016-12-09 14:10:14 +0400

    Cleaned up. Done LINQ part 2 and 3.

--- Linq/Task/LinqSamples.cs
+++ Linq/Task/LinqSamples.cs
@@ -1,29 +1,25 @@
 ﻿// Copyright © Microsoft Corporation.  All Rights Reserved.
-// This code released under the terms of the 
+// This code released under the terms of the
 // Microsoft Public License (MS-PL, http://opensource.org/licenses/ms-pl.html.)
 //
 //Copyright (C) Microsoft Corporation.  All rights reserved.
 
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Windows.Forms;
-using System.Xml.Linq;
 using SampleSupport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
 using Task.Data;
 
 // Version Mad01
 
 namespace SampleQueries
 {
-	[Title("LINQ Module")]
-	[Prefix("Linq")]
-	public class LinqSamples : SampleHarness
-	{
-
-		private DataSource dataSource = new DataSource();
+    [Title("LINQ Module")]
+    [Prefix("Linq")]
+    public class LinqSamples : SampleHarness
+    {
+        private DataSource dataSource = new DataSource();
 
         //[Category("Restriction Operators")]
         //[Title("Where - Task 1")]
@@ -66,68 +62,77 @@ namespace SampleQueries
         [Description("Выдайте список всех клиентов, чей суммарный оборот (сумма всех заказов) превосходит некоторую величину X. Продемонстрируйте выполнение запроса с различными X (подумайте, можно ли обойтись без копирования запроса несколько раз)")]
         public void Linq001()
         {
-            int[] arr = {100, 1000, 10000, 100000};
+            int[] arr = { 100, 1000, 10000, 100000 };
 
             foreach (var x in arr)
             {
-                ObjectDumper.Write("");
-                ObjectDumper.Write("");
-                ObjectDumper.Write($"x is {x}");
-                ObjectDumper.Write("");
-                ObjectDumper.Write("");
+                LinqSamples.WriteHeader($"x is {x}");
 
                 var products = this.dataSource.Customers
                     .Where(cust => cust.Orders.Sum(order => order.Total) > x)
-                    .Select(cust => $"Name = {cust.CompanyName}, Order's total price = {cust.Orders.Sum(order => order.Total)}" );
+                    .Select(cust => $"Name = {cust.CompanyName}, Order's total price = {cust.Orders.Sum(order => order.Total)}");
 
                 LinqSamples.Show(products);
             }
         }
 
         [Category("Restriction Operators")]
-	    [Title("Task 2")]
-	    [Description("Для каждого клиента составьте список поставщиков, находящихся в той же стране и том же городе. Сделайте задания с использованием группировки и без")]
-	    public void Linq002()
+        [Title("Task 2")]
+        [Description("Для каждого клиента составьте список поставщиков, находящихся в той же стране и том же городе. Сделайте задания с использованием группировки и без")]
+        public void Linq002()
         {
-            var clients = this.dataSource.Customers;
+            var сustomers = this.dataSource.Customers;
             var suppliers = this.dataSource.Suppliers;
 
             var result =
                 from supplier in suppliers
-                from client in clients
-                where supplier.Country == client.Country &&
-                      supplier.City == client.City
-                select $"Company: {client.CompanyName}, city: {supplier.City}, country: {supplier.Country}";
+                from customer in сustomers
+                where supplier.Country == customer.Country &&
+                      supplier.City == customer.City
+                select $"Company {customer.CompanyName} and supplier {supplier.SupplierName} are in the same city {supplier.City} and country {supplier.Country}";
 
-            ObjectDumper.Write("");
-            ObjectDumper.Write("");
-            ObjectDumper.Write("Ingroupped");
-            ObjectDumper.Write("");
-            ObjectDumper.Write("");
-
+            LinqSamples.WriteHeader("Ingroupped");
+            
             LinqSamples.Show(result);
 
             var grouppedResult =
-                from supplier in suppliers
-                from client in clients
-                where supplier.Country == client.Country &&
-                      supplier.City == client.City
-                group client by client.Country;
+                from grouppedCollection in (
+                    from supplier in suppliers
+                    from customer in сustomers
+                    where supplier.Country == customer.Country &&
+                          supplier.City == customer.City
+                    select new
+                    {
+                        Customer = customer,
+                        Supplier = supplier,
+                    })
+                group grouppedCollection by grouppedCollection.Customer.Country;
 
-            ObjectDumper.Write("");
-            ObjectDumper.Write("");
-            ObjectDumper.Write("Groupped");
-            ObjectDumper.Write("");
-            ObjectDumper.Write("");
+            LinqSamples.WriteHeader("Groupped");
 
-            LinqSamples.Show(grouppedResult);
+            if (grouppedResult.Any())
+            {
+                foreach (var p in grouppedResult)
+                {
+                    ObjectDumper.Write($"In {p.Key} there's");
+
+                    foreach (var item in p)
+                    {
+                        ObjectDumper.Write($"    company {item.Customer.CompanyName} and supplier {item.Supplier.SupplierName}");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("No such objects");
+            }
         }
 
         [Category("Restriction Operators")]
-	    [Title("Task 3")]
-	    [Description("Найдите всех клиентов, у которых были заказы, превосходящие по сумме величину X")]
-	    public void Linq003()
-	    {
+        [Title("Task 3")]
+        [Description("Найдите всех клиентов, у которых были заказы, превосходящие по сумме величину X")]
+        public void Linq003()
+        {
             int[] arr = { 100, 1000, 10000, 100000 };
 
             foreach (var x in arr)
@@ -140,17 +145,17 @@ namespace SampleQueries
 
                 var result = this.dataSource.Customers
                     .Where(cust => cust.Orders.Any(order => order.Total > x))
-                    .Select(cust => cust.CompanyName);
+                    .Select(cust => $"Customer {cust.CompanyName} have order with {cust.Orders.First(order => order.Total > x).Total} total");
 
                 LinqSamples.Show(result);
             }
         }
 
-	    [Category("Restriction Operators")]
-	    [Title("Task 4")]
-	    [Description("Выдайте список клиентов с указанием, начиная с какого месяца какого года они стали клиентами (принять за таковые месяц и год самого первого заказа)")]
-	    public void Linq004()
-	    {
+        [Category("Restriction Operators")]
+        [Title("Task 4")]
+        [Description("Выдайте список клиентов с указанием, начиная с какого месяца какого года они стали клиентами (принять за таковые месяц и год самого первого заказа)")]
+        public void Linq004()
+        {
             var result = this.dataSource.Customers
                 .Select(cust => $"{cust.CompanyName} is client since {cust.Orders.OrderBy(order => order.OrderDate).Select(order => order.OrderDate.ToString("MMMM yyyy")).FirstOrDefault()}");
 
@@ -175,12 +180,12 @@ namespace SampleQueries
             LinqSamples.Show(result);
         }
 
-	    [Category("Restriction Operators")]
-	    [Title("Task 6")]
-	    [Description("Укажите всех клиентов, у которых указан нецифровой почтовый код или не заполнен регион или в телефоне не указан код оператора (для простоты считаем, что это равнозначно «нет круглых скобочек в начале»).")]
-	    public void Linq006()
-	    {
-	        var result = this.dataSource.Customers
+        [Category("Restriction Operators")]
+        [Title("Task 6")]
+        [Description("Укажите всех клиентов, у которых указан нецифровой почтовый код или не заполнен регион или в телефоне не указан код оператора (для простоты считаем, что это равнозначно «нет круглых скобочек в начале»).")]
+        public void Linq006()
+        {
+            var result = this.dataSource.Customers
                 .Where(cust => string.IsNullOrWhiteSpace(cust.PostalCode) ||
                     cust.PostalCode.Any(c => !char.IsDigit(c)) ||
                     string.IsNullOrWhiteSpace(cust.Region) ||
@@ -190,30 +195,30 @@ namespace SampleQueries
             LinqSamples.Show(result);
         }
 
-	    [Category("Restriction Operators")]
-	    [Title("Task 7")]
-	    [Description("Сгруппируйте все продукты по категориям, внутри – по наличию на складе, внутри последней группы отсортируйте по стоимости")]
-	    public void Linq007()
-	    {
-	        var result =
-	            from product in this.dataSource.Products
-	            group product by product.Category
-	            into groupedByCategoryProducts
-	            select
-	                new KeyValuePair<string, IEnumerable<KeyValuePair<decimal, IEnumerable<Product>>>>(
-	                    groupedByCategoryProducts.Key,
-	                    from p in groupedByCategoryProducts
-	                    group p by p.UnitsInStock
-	                    into asd
-	                    select new KeyValuePair<decimal, IEnumerable<Product>>(asd.Key, asd.OrderBy(a => a.UnitPrice)));
+        [Category("Restriction Operators")]
+        [Title("Task 7")]
+        [Description("Сгруппируйте все продукты по категориям, внутри – по наличию на складе, внутри последней группы отсортируйте по стоимости")]
+        public void Linq007()
+        {
+            var result =
+                from product in this.dataSource.Products
+                group product by product.Category
+                into groupedByCategoryProducts
+                select
+                    new KeyValuePair<string, IEnumerable<KeyValuePair<decimal, IEnumerable<Product>>>>(
+                        groupedByCategoryProducts.Key,
+                        from p in groupedByCategoryProducts
+                        group p by p.UnitsInStock
+                        into asd
+                        select new KeyValuePair<decimal, IEnumerable<Product>>(asd.Key, asd.OrderBy(a => a.UnitPrice)));
 
             LinqSamples.Show(result);
         }
 
-	    [Category("Restriction Operators")]
-	    [Title("Task 8")]
-	    [Description("Сгруппируйте товары по группам «дешевые», «средняя цена», «дорогие». Границы каждой группы задайте сами")]
-	    public void Linq008()
+        [Category("Restriction Operators")]
+        [Title("Task 8")]
+        [Description("Сгруппируйте товары по группам «дешевые», «средняя цена», «дорогие». Границы каждой группы задайте сами")]
+        public void Linq008()
         {
             const int min = 10;
             const int max = 100;
@@ -225,48 +230,47 @@ namespace SampleQueries
             LinqSamples.Show(result);
         }
 
-	    [Category("Restriction Operators")]
-	    [Title("Task 9")]
-	    [Description("Рассчитайте среднюю прибыльность каждого города (среднюю сумму заказа по всем клиентам из данного города) и среднюю интенсивность (среднее количество заказов, приходящееся на клиента из каждого города)")]
-	    public void Linq009()
-	    {
-	        double avgIntensivity = this.dataSource.Customers
+        [Category("Restriction Operators")]
+        [Title("Task 9")]
+        [Description("Рассчитайте среднюю прибыльность каждого города (среднюю сумму заказа по всем клиентам из данного города) и среднюю интенсивность (среднее количество заказов, приходящееся на клиента из каждого города)")]
+        public void Linq009()
+        {
+            double avgIntensivity = this.dataSource.Customers
                 .Where(customer => customer.Orders.Any())
                 .Average(customer => customer.Orders.Length);
 
-	        decimal avgMoneys = this.dataSource.Customers
+            decimal avgMoneys = this.dataSource.Customers
                 .Where(customer => customer.Orders.Any())
-	            .Average(customer => customer.Orders.Average(order => order.Total));
+                .Average(customer => customer.Orders.Average(order => order.Total));
 
             ObjectDumper.Write(avgIntensivity);
             ObjectDumper.Write(avgMoneys);
         }
 
-	    [Category("Restriction Operators")]
-	    [Title("Task 10")]
-	    [Description("Сделайте среднегодовую статистику активности клиентов по месяцам (без учета года), статистику по годам, по годам и месяцам (т.е. когда один месяц в разные годы имеет своё значение).")]
-	    public void Linq010()
-	    {
-	        
-	    }
+        [Category("Restriction Operators")]
+        [Title("Task 10")]
+        [Description("Сделайте среднегодовую статистику активности клиентов по месяцам (без учета года), статистику по годам, по годам и месяцам (т.е. когда один месяц в разные годы имеет своё значение).")]
+        public void Linq010()
+        {
+        }
 
         private static string GetRange(Product product, int a, int b)
-	    {
-	        return product.UnitPrice < a
-	            ? "Low"
-	            : product.UnitPrice < b
-	                ? "Middle"
-	                : "High";
-	    }
+        {
+            return product.UnitPrice < a
+                ? "Low"
+                : product.UnitPrice < b
+                    ? "Middle"
+                    : "High";
+        }
 
-	    private static void Show(IEnumerable<IGrouping<string, KeyValuePair<string, string>>> products)
-	    {
+        private static void Show(IEnumerable<IGrouping<string, KeyValuePair<string, string>>> products)
+        {
             if (products.Any())
             {
                 foreach (var group in products)
                 {
                     ObjectDumper.Write(group.Key);
-                    
+
                     foreach (var pair in group)
                     {
                         ObjectDumper.Write($"    {pair.Key} : {pair.Value}");
@@ -279,9 +283,8 @@ namespace SampleQueries
             }
         }
 
-
-	    private static void Show(IEnumerable<KeyValuePair<string, IEnumerable<KeyValuePair<decimal, IEnumerable<Product>>>>> products)
-	    {
+        private static void Show(IEnumerable<KeyValuePair<string, IEnumerable<KeyValuePair<decimal, IEnumerable<Product>>>>> products)
+        {
             if (products.Any())
             {
                 foreach (var p in products)
@@ -305,8 +308,8 @@ namespace SampleQueries
             }
         }
 
-	    private static void Show(IOrderedEnumerable<KeyValuePair<Customer, Order>> products)
-	    {
+        private static void Show(IOrderedEnumerable<KeyValuePair<Customer, Order>> products)
+        {
             if (products.Any())
             {
                 foreach (var p in products)
@@ -320,19 +323,18 @@ namespace SampleQueries
             }
         }
 
-	    private static void Show(IEnumerable<IGrouping<string, object>> products)
-	    {
+        private static void Show(IEnumerable<IGrouping<string, string>> products)
+        {
             if (products.Any())
             {
                 foreach (var p in products)
                 {
                     ObjectDumper.Write(p.Key);
 
-                    foreach (object item in p)
+                    foreach (var item in p)
                     {
                         ObjectDumper.Write("    " + item);
                     }
-
                 }
             }
             else
@@ -341,7 +343,7 @@ namespace SampleQueries
             }
         }
 
-	    private static void Show(IEnumerable<string> products)
+        private static void Show(IEnumerable<string> products)
         {
             if (products.Any())
             {
@@ -354,6 +356,15 @@ namespace SampleQueries
             {
                 Console.WriteLine("No such objects");
             }
+        }
+
+        private static void WriteHeader(string v)
+        {
+            ObjectDumper.Write("");
+            ObjectDumper.Write("");
+            ObjectDumper.Write(v);
+            ObjectDumper.Write("");
+            ObjectDumper.Write("");
         }
     }
 }
