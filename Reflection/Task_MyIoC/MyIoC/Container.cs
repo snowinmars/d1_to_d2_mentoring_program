bfab002d5cbb03600e71ad3c7a9296f9b83bfd11
Dmitrii_Tkachenko <Dmitrii_Tkachenko@epam.com>
2016-12-21 14:21:47 +0400

    Started Reflection task impl.

--- Reflection/Task_MyIoC/MyIoC/Container.cs
+++ Reflection/Task_MyIoC/MyIoC/Container.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SandS.Algorithm.Extensions.EnumerableExtensionNamespace;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -9,14 +10,36 @@ namespace MyIoC
 {
 	public class Container
 	{
-		public void AddAssembly(Assembly assembly)
-		{ }
+	    public Container()
+	    {
+	        this.Types = new List<Type>();
+	    }
 
-		public void AddType(Type type)
-		{ }
+	    private IList<Type> Types { get; }
 
-		public void AddType(Type type, Type baseType)
-		{ }
+	    public void AddAssembly(Assembly assembly)
+	    {
+	        assembly.GetTypes().ForEach(this.AddType);
+	    }
+
+        private void HandleTypeEntry(Type type)
+        {
+            var imports = type.GetCustomAttributes(typeof(ImportAttribute), inherit: true);
+            var importsCtors = type.GetCustomAttributes(typeof(ImportConstructorAttribute), inherit: true);
+
+            this.Types.Add(type);
+        }
+
+        public void AddType(Type type)
+	    {
+            this.HandleTypeEntry(type);
+        }
+
+	    public void AddType(Type type, Type baseType)
+	    {
+	        this.AddType(type);
+            this.AddType(baseType);
+	    }
 
 		public object CreateInstance(Type type)
 		{
@@ -29,17 +52,25 @@ namespace MyIoC
 		}
 
 
-		public void Sample()
+		public static void AssemblySample()
 		{
 			var container = new Container();
 			container.AddAssembly(Assembly.GetExecutingAssembly());
 
-			var customerBLL = (CustomerBLL)container.CreateInstance(typeof(CustomerBLL));
-			var customerBLL2 = container.CreateInstance<CustomerBLL>();
-
-			container.AddType(typeof(CustomerBLL));
-			container.AddType(typeof(Logger));
-			container.AddType(typeof(CustomerDAL), typeof(ICustomerDAL));
+			var customerBLL = (CustomerBll)container.CreateInstance(typeof(CustomerBll));
+			var customerBLL2 = container.CreateInstance<CustomerBll>();
 		}
-	}
+
+        public static void DirectSample()
+        {
+            var container = new Container();
+
+            container.AddType(typeof(CustomerBll));
+            container.AddType(typeof(Logger));
+            container.AddType(typeof(CustomerDao), typeof(ICustomerDao));
+
+            var customerBll = (CustomerBll)container.CreateInstance(typeof(CustomerBll));
+            var customerBll2 = container.CreateInstance<CustomerBll>();
+        }
+    }
 }
