6b8d155177c539821ebb14d0179d6c8f84e1ec88
Dmitrii_Tkachenko <Dmitrii_Tkachenko@epam.com>
2017-01-19 11:50:28 +0400

    Refactored.

--- Basic XML/BasicXml/BasicXml/Library.cs
+++ Basic XML/BasicXml/BasicXml/Library.cs
@@ -8,20 +8,6 @@ using System.Xml;
 
 namespace BasicXml
 {
-    public static class TypeExtensions
-    {
-        public static object GetDefaultValue(this Type t)
-        {
-            if (t.IsValueType &&
-                Nullable.GetUnderlyingType(t) == null)
-            {
-                return Activator.CreateInstance(t);
-            }
-
-            return null;
-        }
-    }
-
     public static class Library
     {
         private static string FullPathToDataFile = @"D:\file.dat";
---
+++ Basic XML/BasicXml/BasicXml/TypeExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicXml
+{
+    public static class TypeExtensions
+    {
+        public static object GetDefaultValue(this Type t)
+        {
+            if (t.IsValueType &&
+                Nullable.GetUnderlyingType(t) == null)
+            {
+                return Activator.CreateInstance(t);
+            }
+
+            return null;
+        }
+    }
+}
