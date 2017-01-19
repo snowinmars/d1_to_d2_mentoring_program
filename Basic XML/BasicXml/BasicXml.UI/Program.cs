ffabe7ac31e6ac4443040f4af46b875195b5fbac
Dmitrii_Tkachenko <Dmitrii_Tkachenko@epam.com>
2017-01-19 11:49:10 +0400

    Refactored.

--- Basic XML/BasicXml/BasicXml.UI/Program.cs
+++ Basic XML/BasicXml/BasicXml.UI/Program.cs
@@ -3,16 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BasicXml.Common;
 
 namespace BasicXml.UI
 {
     class Program
     {
+        public static Book GetNewBook()
+        {
+            return new Book
+            {
+                Authors = { Author.GetNewOne(), Author.GetNewOne(), Author.GetNewOne() },
+                Annotation = string.Concat(Constants.TextGenerator.GetWords(20)),
+                CityName = Constants.TextGenerator.GetNewWord(3, 12, isFirstLerretUp: true),
+                Isbn = Constants.Isbn10Generator.Generate(),
+                Year = Constants.Random.Next(1800, 2017),
+                Id = Guid.NewGuid(),
+                PageNumber = Constants.Random.Next(10, 1337),
+                Title = Constants.TextGenerator.GetNewWord(3, 12, isFirstLerretUp: true),
+            };
+        }
+
         static void Main(string[] args)
         {
-            Library.AddAll(new[] {Book.GetNewOne(), Book.GetNewOne() , Book.GetNewOne() , Book.GetNewOne() , Book.GetNewOne() });
+            Library.AddAll(new[] {Program.GetNewBook(), Program.GetNewBook() , Program.GetNewBook() , Program.GetNewBook() , Program.GetNewBook() });
 
-            Library.GetAll();
+            var collection = Library.GetAll();
         }
     }
 }
--- Basic XML/BasicXml/BasicXml/Book.cs
+++ Basic XML/BasicXml/BasicXml/Book.cs
@@ -14,21 +14,6 @@ namespace BasicXml
             this.Authors = new List<Author>();
         }
 
-        public static Book GetNewOne()
-        {
-            return new Book
-            {
-                Authors = {Author.GetNewOne(), Author.GetNewOne(), Author.GetNewOne()},
-                Annotation = string.Concat(Constants.TextGenerator.GetWords(20)),
-                CityName = Constants.TextGenerator.GetNewWord(3, 12, isFirstLerretUp: true),
-                Isbn = Constants.Isbn10Generator.Generate(),
-                Year = Constants.Random.Next(1800, 2017),
-                Id = Guid.NewGuid(),
-                PageNumber = Constants.Random.Next(10, 1337),
-                Title = Constants.TextGenerator.GetNewWord(3, 12, isFirstLerretUp: true),
-            };
-        }
-
         public IList<Author> Authors { get; }
 
         public string Annotation { get; set; }
